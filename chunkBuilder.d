8e86dc2c946033d581dbe38a7f98933d89671aeb
Th3Fi <156732301+Th3Fi@users.noreply.github.com>
2026-05-09 09:38:34 -0500

    Update chunkBuilder.d
    Got the parallelization to function correctly using task, still need to work out the kinks with "workerInit()"

--- chunkBuilder.d
+++ chunkBuilder.d
@@ -4,7 +4,8 @@ import core.thread;
 
 void workerInit(int workerId){
     int stopPoint = workerId - 2;
-    for(int[3] chunkDimensions = [workerId, 250, 16]; chunkDimensions == [stopPoint, 0, 0];){
+    for(int[3] chunkDimensions = [workerId, 250, 16]; chunkDimensions == [stopPoint, 0, 0];){ //Will work on this later, it does not execute the for loop at this time.
+        writeln(chunkDimensions);
         if (chunkDimensions[1] > 0){
             --chunkDimensions[1];
             writeln(chunkDimensions);
@@ -19,6 +20,7 @@ void workerInit(int workerId){
             writeln(chunkDimensions);
         }
     }
+    writeln("core id: ", workerId, " | stop point: ", stopPoint); // stops execution here
 }
 
 
@@ -26,14 +28,18 @@ int coreCount(){
     int coreUsage = 3;
     coreUsage = ((totalCPUs <= 12) && (totalCPUs > 7)) ? 8 : coreUsage;
     coreUsage = (totalCPUs >= 16) ? 16 : coreUsage;
+    writeln("core usage: ", coreUsage);
     return (coreUsage); //this recalculates coresUsage on call using more CPU but less RAM
 }
 
 void chunkBuild(int cores){
     int workLoad = 16/cores;
-    for(int workerCount; workerCount != 16; workerCount += workLoad){
+    writeln("work load: ", workLoad);
+    for(int workerCount; workerCount <= 16;){
         workerCount += workLoad;
         auto worker = task!workerInit(workerCount);
+        worker.executeInNewThread();
+        writeln("worker count: ", workerCount);
     }
 }
 
