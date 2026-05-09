020daa876ccfb9324e20e7eb663fed94c6a1e7fa
Th3Fi <156732301+Th3Fi@users.noreply.github.com>
2026-05-09 08:47:10 -0500

    Wrote some more lines to chunk builder
    more stuff to parallel process the chunk functions

--- chunkBuilder.d
+++ chunkBuilder.d
@@ -1,21 +1,42 @@
 import std.stdio;
 import std.parallelism;
+import core.thread;
 
-struct chunkBuild{
-
+void workerInit(int workerId){
+    int stopPoint = workerId - 2;
+    for(int[3] chunkDimensions = [workerId, 250, 16]; chunkDimensions == [stopPoint, 0, 0];){
+        if (chunkDimensions[1] > 0){
+            --chunkDimensions[1];
+            writeln(chunkDimensions);
+        } else if (chunkDimensions[2] > 0){
+            --chunkDimensions[0];
+            chunkDimensions[0] = 250;
+            writeln(chunkDimensions);
+        } else if (chunkDimensions[2] == 0){
+            --chunkDimensions[1];
+            chunkDimensions[0] = 250;
+            chunkDimensions[2] = 16;
+            writeln(chunkDimensions);
+        }
+    }
 }
+
 
 int coreCount(){
-    int coreUsage = 4;
-    coreUsage = (totalCPUs > 4) ? (coreUsage = totalCPUs - 4) : coreUsage;
-    writeln(coreUsage); //this recalculates coresUsage on call using more CPU but less RAM
-    return (coreUsage);
+    int coreUsage = 3;
+    coreUsage = ((totalCPUs <= 12) && (totalCPUs > 7)) ? 8 : coreUsage;
+    coreUsage = (totalCPUs >= 16) ? 16 : coreUsage;
+    return (coreUsage); //this recalculates coresUsage on call using more CPU but less RAM
 }
 
-void chunkBuild(){
-
+void chunkBuild(int cores){
+    int workLoad = 16/cores;
+    for(int workerCount; workerCount != 16; workerCount += workLoad){
+        workerCount += workLoad;
+        auto worker = task!workerInit(workerCount);
+    }
 }
 
 void main(){
-
+    chunkBuild(coreCount());
 }
