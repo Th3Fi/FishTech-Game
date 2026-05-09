45b0581d0961d84a1c757e734c8fa07baf9a0011
Th3Fi <156732301+Th3Fi@users.noreply.github.com>
2026-05-09 18:15:40 -0500

    The begining of the C# game engine
    Uses OpenTK as the graphics binding
    I dont know enough C# to make this thing do much
    Once I do know enough I will write data parallelization systems on ILGPU

---
+++ OceanicEngine/Program.cs
@@ -0,0 +1,22 @@
+﻿using OpenTK.Graphics.OpenGL4;
+using OpenTK.Windowing.Common;
+using OpenTK.Windowing.Desktop;
+using OpenTK.Windowing.GraphicsLibraryFramework;
+
+namespace OceanicEngineMain
+{
+    public class Game : GameWindow
+    {
+        public Game(int width, int height, string title) : base(GameWindowSettings.Default, new NativeWindowSettings() { ClientSize = (width, height), Title = title })
+        {
+        }
+
+        static void Main()
+        {
+            using (Game game = new Game(800, 600, "FishTech"))
+            {
+                game.Run();
+            }
+        }
+    }
+}
